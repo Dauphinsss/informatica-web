bc65201adcc229ca7a9224437e9b9ac58438cb35
Marko <marcosvelasquezvela123@gmail.com>
2026-02-08 16:35:58 -0400

    test deploy

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -15,6 +15,8 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
   )
 }
 
+
+//deploid test
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
